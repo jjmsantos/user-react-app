8d42e374a72e22142b1a2cfa82f683047327ac58
JSantos <jeremy.santos1010@gmail.com>
2021-08-20 19:46:55 -0400

    used useState to grab entered input fields intp the AddUserHandler function. value currently just logs to console

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,21 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Card from "../UI/Card";
 import Button from "../UI/Button";
 import classes from "./AddUser.module.css";
 
 const AddUser = (props) => {
+	const [enteredUsername, setEnteredUsername] = useState("");
+	const [enteredAge, setEnteredAge] = useState("");
+
 	const AddUserHandler = (event) => {
 		event.preventDefault();
+		console.log(enteredUsername, enteredAge);
+	};
+
+	const usernameChangeHandler = (event) => {
+		setEnteredUsername(event.target.value);
+	};
+
+	const ageChangeHandler = (event) => {
+		setEnteredAge(event.target.value);
 	};
 
 	return (
 		<Card className={classes.input}>
 			<form onSubmit={AddUserHandler}>
 				<label htmlFor="username">Username</label>
-				<input type="text" id="username" />
+				<input type="text" id="username" onChange={usernameChangeHandler} />
 				<label htmlFor="age">Age</label>
-				<input type="number" id="age" />
+				<input type="number" id="age" onChange={ageChangeHandler} />
 				<Button type="submit"> Add User</Button>
 			</form>
 		</Card>
